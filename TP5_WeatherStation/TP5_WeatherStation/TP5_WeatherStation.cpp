c47550da9aca5dd27f563f8473e62cd3b456b9b7
Suela Tafa <rakel.tafa@gmail.com>
2022-12-12 14:42:38 +0100

    Jusqu'à QJsonObject mainObj

--- TP5_WeatherStation/TP5_WeatherStation/TP5_WeatherStation.cpp
+++ TP5_WeatherStation/TP5_WeatherStation/TP5_WeatherStation.cpp
@@ -22,30 +22,15 @@ TP5_WeatherStation::TP5_WeatherStation(DbManager *dbm, QWidget* parent)
 {
     ui->setupUi(this);
 
-
-    // Pour tester les entêtes HTTP
-    QString URL = "https://api.openweathermap.org/data/2.5/weather?q=bourg-enbresse,
-    fr & units = metric & lang = fr & appid = xxxx";
-    QUrl url(URL);
-    QNetworkRequest request;
-    request->setUrl(url);
-    //--header ’Accept: application/json’
-    request->setRawHeader("Accept", "application/json");
-    qDebug() << Q_FUNC_INFO << request->url();
-    netmanager->get(request);
-        
-
     // Weather report View
     reportView = new ViewReport(weatherReport,ui);
     // Pollution Forecast View
     pollutionView = new ViewPollution(dbmanager, ui->groupBox_pollution);
 
     // netmanager here (or better in initialisation list)  + callback to replyFinished
+    netmanager = new QNetworkAccessManager(this);
 
     weatherRequest();
-    netmanager = new QNetworkAccessManager(this);
-    connect(netmanager, SIGNAL(finished(QNetworkReply*)),
-        this, SLOT(weatherReplyFinished(QNetworkReply*)));
     // uncomment once observable implemented
 //    connect(ui->pushButton_weather_request, &QPushButton::pressed, this, &TP5_WeatherStation::weatherRequest);
 //    connect(ui->pushButton_weather_request, &QPushButton::pressed, this, &TP5_WeatherStation::pollutionRequest);
@@ -63,13 +48,23 @@ TP5_WeatherStation::~TP5_WeatherStation()
 void TP5_WeatherStation::weatherRequest() {
 
     // your request here
+    QString URL = "https://api.openweathermap.org/data/2.5/weather?q=bourg-en-bresse,fr&units=metric&lang=fr&appid=0d41ac5cb088db1d5301807cbfb18170";
+    QUrl url(URL);
+    QNetworkRequest* request = new QNetworkRequest(url);
+    request->setUrl(url);
 
+    //--header ’Accept: application/json’
+    request->setRawHeader("Accept", "application/json");
+    qDebug() << Q_FUNC_INFO << request->url();
+    netmanager->get(*request);
 }
 
 void TP5_WeatherStation::weatherReplyFinished(QNetworkReply* reply)
 {
-    // managing response here (with errors handling if possible)
-
-    //don't forget to delete when no more requested
+    QByteArray datas = reply->readAll();
+    QJsonDocument jsonResponse = QJsonDocument::fromJson(datas);
+    QJsonObject jsonObj = jsonResponse.object();
+    QString infos(datas);
+    //...
     reply->deleteLater();
 }
